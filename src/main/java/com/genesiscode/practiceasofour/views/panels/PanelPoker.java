602405b696b7c8305913490f63149c237bda8ebb
Jose Maria <josmariaguilar@gmail.com>
2021-11-25 08:56:38 -0400

    bottom panel implemented.

--- src/main/java/com/genesiscode/practiceasofour/views/panels/PanelPoker.java
+++ src/main/java/com/genesiscode/practiceasofour/views/panels/PanelPoker.java
@@ -1,18 +1,21 @@
 package com.genesiscode.practiceasofour.views.panels;
 
 import com.genesiscode.practiceasofour.views.panels.commons.PanelsCommons;
+import com.genesiscode.practiceasofour.views.panels.rows.RowPoker;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class PanelPoker extends Panel {
 
     private static PanelPoker panelPoker;
 
-    //Panel main
     private Pane paneMain;
 
     //Controls by top left panel
@@ -24,6 +27,12 @@ public class PanelPoker extends Panel {
     //Controls by top right panel
     private static TextField txtLevelAcceptance;
     private static Button btnStart;
+
+    //Controls by bottom left panel
+    private static TableView<RowPoker> tableData;
+
+    //Controls by bottom right panel
+    private static Label lblValueToResult, lblResult;
 
     private PanelPoker() {
         super("Prueba de poker");
@@ -73,8 +82,13 @@ public class PanelPoker extends Panel {
         btnStart = new Button("Comenzar");
         btnStart.setOnAction(actionEvent -> click_btn_start());
 
-        //pane bottom left
+        //bottom left panel
+        tableData = new TableView<>();
+        tableData.setMaxHeight(300);
+
         //pane bottom right
+        lblValueToResult = new Label();
+        lblResult = new Label();
     }
 
     private void buildPanel() {
@@ -84,7 +98,14 @@ public class PanelPoker extends Panel {
 
         HBox topPanel = new HBox(10, topLeftPanel, topRightPanel());
         topPanel.setAlignment(Pos.CENTER);
-        this.paneMain = topPanel;
+
+        HBox bottomPanel = new HBox(10, bottomLeftPanel(), bottomRightPanel());
+        bottomPanel.setAlignment(Pos.CENTER);
+
+        VBox paneMain = new VBox(topPanel, bottomPanel);
+        paneMain.setAlignment(Pos.CENTER);
+
+        this.paneMain = paneMain;
     }
 
     private Pane radioButtonsPanel() {
@@ -107,6 +128,46 @@ public class PanelPoker extends Panel {
         HBox paneAcceptance = new HBox(10, new Label("Nivel de aceptación"), txtLevelAcceptance);
         paneAcceptance.setAlignment(Pos.CENTER);
         VBox pane = new VBox(10, paneAcceptance, btnStart);
+        pane.setAlignment(Pos.CENTER);
+        return pane;
+    }
+
+    private VBox bottomLeftPanel() {
+        createTableData();
+        VBox panel = new VBox(tableData);
+        panel.setAlignment(Pos.CENTER);
+        panel.setPadding(new Insets(10));
+        return panel;
+    }
+
+    private void createTableData() {
+        TableColumn<RowPoker, String> colCategory = new TableColumn<>("Categoria");
+        colCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+        colCategory.setPrefWidth(150);
+
+        TableColumn<RowPoker, Double> colProbability = new TableColumn<>("Probabilidad");
+        colProbability.setCellValueFactory(new PropertyValueFactory<>("probability"));
+        colProbability.setPrefWidth(100);
+
+        TableColumn<RowPoker, Integer> colOi = new TableColumn<>("Oi");
+        colOi.setCellValueFactory(new PropertyValueFactory<>("oi"));
+        colOi.setPrefWidth(50);
+
+        TableColumn<RowPoker, String> colEi = new TableColumn<>("Ei");
+        colEi.setCellValueFactory(new PropertyValueFactory<>("ei"));
+        colEi.setPrefWidth(150);
+
+        TableColumn<RowPoker, Double> colResult = new TableColumn<>("(FEi-FOi)²/FEi");
+        colResult.setCellValueFactory(new PropertyValueFactory<>("result"));
+        colResult.setPrefWidth(120);
+
+        tableData.getColumns().addAll(List.of(colCategory, colProbability, colOi, colEi, colResult));
+    }
+
+    private VBox bottomRightPanel() {
+        Label lblTitleByFinalResult = new Label("RESULTADO FINAL");
+        Label lblTitle = new Label("X²o < x²α,m-1");
+        VBox pane = new VBox(10, lblTitleByFinalResult, lblTitle, lblValueToResult, lblResult);
         pane.setAlignment(Pos.CENTER);
         return pane;
     }
---
+++ src/main/java/com/genesiscode/practiceasofour/views/panels/rows/RowPoker.java
@@ -0,0 +1,38 @@
+package com.genesiscode.practiceasofour.views.panels.rows;
+
+public class RowPoker {
+
+    private final String category;
+    private final double probability;
+    private final int oi;
+    private final String ei;
+    private final double result;
+
+    public RowPoker(String category, double probability, int oi, String ei, double result) {
+        this.category = category;
+        this.probability = probability;
+        this.oi = oi;
+        this.ei = ei;
+        this.result = result;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public int getOi() {
+        return oi;
+    }
+
+    public String getEi() {
+        return ei;
+    }
+
+    public double getResult() {
+        return result;
+    }
+}
